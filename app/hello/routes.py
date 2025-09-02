8a2cc79e2e0f0d9497b8ee2011c204154ff51a71
Matheus <matheus.foscarinid@gmail.com>
2025-09-01 21:53:56 -0300

    database changes

--- app/hello/routes.py
+++ app/hello/routes.py
@@ -1,11 +1,14 @@
-from flask import Blueprint
+from flask import Blueprint, render_template
 
 hello_bp = Blueprint('hello', __name__)
 
 @hello_bp.route('/')
 def index():
-    return "Olá, Mundo!"
+    usuarios = ['Matheus', 'João', 'Maria']
+    return render_template('index.html', usuarios=usuarios)
 
 @hello_bp.route('/sobre')
 def about():
     return "Olá, Matheus!"
+
+
